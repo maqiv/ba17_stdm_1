df226c9b9e74f0bff01bc36661c8badf86e4dc87
s <sebastian.glinski@gmail.com>
2017-05-31 18:04:48 +0200

    cnn_rnn_tf_3 verification run with one dense layer

--- src/network_runner.py
+++ src/network_runner.py
@@ -200,3 +200,75 @@ crt3.cnn_rnn_tf_3('nets/crt_settings.json',
                     n_strides2=[2, 1],
                     n_dense1=200
                 )
+
+crt3.cnn_rnn_tf_3('nets/crt_settings.json',
+                    n_filter1=32,
+                    n_kernel1=[8, 1],
+                    n_pool1=[4, 4],
+                    n_strides1=[2, 1],
+                    n_filter2=64,
+                    n_kernel2=[6, 1],
+                    n_pool2=[3, 3],
+                    n_strides2=[2, 1],
+                    n_dense1=175
+                )
+
+crt3.cnn_rnn_tf_3('nets/crt_settings.json',
+                    n_filter1=32,
+                    n_kernel1=[8, 1],
+                    n_pool1=[4, 4],
+                    n_strides1=[2, 1],
+                    n_filter2=64,
+                    n_kernel2=[6, 1],
+                    n_pool2=[3, 3],
+                    n_strides2=[2, 1],
+                    n_dense1=175
+                )
+
+crt3.cnn_rnn_tf_3('nets/crt_settings.json',
+                    n_filter1=32,
+                    n_kernel1=[8, 1],
+                    n_pool1=[4, 4],
+                    n_strides1=[2, 1],
+                    n_filter2=64,
+                    n_kernel2=[6, 1],
+                    n_pool2=[3, 3],
+                    n_strides2=[2, 1],
+                    n_dense1=175
+                )
+
+crt3.cnn_rnn_tf_3('nets/crt_settings.json',
+                    n_filter1=32,
+                    n_kernel1=[8, 1],
+                    n_pool1=[4, 4],
+                    n_strides1=[2, 1],
+                    n_filter2=64,
+                    n_kernel2=[6, 1],
+                    n_pool2=[3, 3],
+                    n_strides2=[2, 1],
+                    n_dense1=150
+                )
+
+crt3.cnn_rnn_tf_3('nets/crt_settings.json',
+                    n_filter1=32,
+                    n_kernel1=[8, 1],
+                    n_pool1=[4, 4],
+                    n_strides1=[2, 1],
+                    n_filter2=64,
+                    n_kernel2=[6, 1],
+                    n_pool2=[3, 3],
+                    n_strides2=[2, 1],
+                    n_dense1=150
+                )
+
+crt3.cnn_rnn_tf_3('nets/crt_settings.json',
+                    n_filter1=32,
+                    n_kernel1=[8, 1],
+                    n_pool1=[4, 4],
+                    n_strides1=[2, 1],
+                    n_filter2=64,
+                    n_kernel2=[6, 1],
+                    n_pool2=[3, 3],
+                    n_strides2=[2, 1],
+                    n_dense1=150
+                )
